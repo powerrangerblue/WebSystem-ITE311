7f0de0e314e403c75d477272445070ce3693e08e
powerrangerblue <canitanjasper016@gmail.com>
2025-09-13 00:04:28 +0800

    Update of USER AUTHENTICATION (LOGIN/REGISTRATION)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use CodeIgniter\Model;
@@ -8,19 +7,52 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
-
+    protected $useAutoIncrement = true;
     protected $returnType = 'array';
-
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
     protected $allowedFields = [
         'name',
         'email',
         'password',
-        'role',
-        'created_at',
-        'updated_at',
+        'role'
     ];
 
-    protected $useTimestamps = false; // timestamps handled by DB defaults in migration
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+
+    // Validation - Only validate fields that exist in your database
+    protected $validationRules = [
+        'name' => 'required|min_length[3]|max_length[100]',
+        'email' => 'required|valid_email|is_unique[users.email]',
+        'password' => 'required|min_length[6]'
+        // Removed role validation since we're setting it programmatically
+    ];
+
+    protected $validationMessages = [
+        'email' => [
+            'is_unique' => 'This email is already registered.'
+        ]
+    ];
+
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = ['hashPassword'];
+    protected $beforeUpdate = ['hashPassword'];
+
+    protected function hashPassword(array $data)
+    {
+        if (!isset($data['data']['password'])) {
+            return $data;
+        }
+
+        $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
+        return $data;
+    }
 }
-
-
